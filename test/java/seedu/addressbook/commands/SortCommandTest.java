f2292c6d5201122bec4add3ca8c0a1366fd6ed4b
Dominic Kenn Lim <dominickenn@gmail.com>
2018-02-14 12:39:11 +0800

    Apply method abstraction to SortCommandTest.

--- test/java/seedu/addressbook/commands/SortCommandTest.java
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -12,23 +12,31 @@ import static org.junit.Assert.assertEquals;
 
 public class SortCommandTest {
 
-    Person andy = TestUtil.generateTestPerson("Andy");
-    Person bob = TestUtil.generateTestPerson("Bob");
-    Person cathy = TestUtil.generateTestPerson("Cathy");
-    Person donald = TestUtil.generateTestPerson("Donald");
-
-    AddressBook addressBook = TestUtil.createAddressBook(bob, andy, donald, cathy);
-    UniquePersonList expectedList = null;
-
-    SortCommand sortCommand = initializeSortCommand();
+    private AddressBook addressBook;
+    private UniquePersonList expectedList;
+    private SortCommand sortCommand;
 
     @Test
     public void execute(){
+        execute_sortCommand_fourPersons();
+    }
+
+    private void execute_sortCommand_fourPersons(){
+        Person andy = TestUtil.generateTestPerson("Andy");
+        Person bob = TestUtil.generateTestPerson("Bob");
+        Person cathy = TestUtil.generateTestPerson("Cathy");
+        Person donald = TestUtil.generateTestPerson("Donald");
+
+        addressBook = TestUtil.createAddressBook(bob, andy, donald, cathy);
+        expectedList = null;
+        sortCommand = initializeSortCommand();
+
         try{
             expectedList = new UniquePersonList(andy, bob, cathy, donald);
         }catch (UniquePersonList.DuplicatePersonException e){
             e.printStackTrace();
         }
+
         assertSortCommandBehavior();
     }
 
